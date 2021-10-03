e7c98f432d21f903f2efaefac0ac84ccdcbb2167
Andrei Sava <andreisava88@gmail.com>
2021-10-03 18:47:21 +0300

    part1 exercises finished

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const DisplayAnecdote = ({ anecdotes, selected, votes }) => {
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
+    </div>
+  );
+};
+
+const MostVoted = ({ anecdotes, votes }) => {
+  const max = Math.max(...votes);
+  const maxIndex = votes.findIndex((vote) => vote === max);
+  if (max === 0) {
+    return <h1>There are no votes at the moment</h1>;
+  }
+
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[maxIndex]}</p>
+      <p>has {max} votes</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients',
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const handleClickAnecdote = () => {
+    const random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  };
+
+  const handleClickVote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
+
+  return (
+    <div>
+      <DisplayAnecdote
+        anecdotes={anecdotes}
+        selected={selected}
+        votes={votes}
+      />
+      <Button onClick={handleClickAnecdote} text='next anecdote' />
+      <Button onClick={handleClickVote} text='vote' />
+
+      <MostVoted anecdotes={anecdotes} votes={votes} />
+    </div>
+  );
+};
+
+export default App;
